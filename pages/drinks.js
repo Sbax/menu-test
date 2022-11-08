6aa8722a324734a01c588705d17d72d6c241a555
Matteo Bacci <matteo.bacci@nearform.com>
2022-11-08 21:31:14 +0100

    Added styles and reduce

--- pages/drinks.js
+++ pages/drinks.js
@@ -1,24 +1,43 @@
 import React from "react";
 import drinks from "../data/drinks.json";
+import styles from "../styles/Drinks.module.css";
 
 function Drink({ drinks }) {
+  const categories = drinks.reduce((categories, { category, ...drink }) => {
+    const found = categories.findIndex(({ name }) => name === category);
+    if (found !== -1) {
+      categories[found].drinks = [...categories[found].drinks, drink];
+      return categories;
+    }
+
+    const newCategory = {
+      name: category,
+      drinks: [drink],
+    };
+
+    return [...categories, newCategory];
+  }, []);
+
   return (
     <div>
-      {drinks.map(({ name, price, category }, index) => (
-        <article key={`drink-${index}`}>
-          <div>
-            Nome: <span>{name}</span>
-          </div>
-          <div>
-            Prezzo: <span>{price}</span>
-          </div>
-          {category && (
-            <div>
-              Categoria: <span>{category}</span>
-            </div>
-          )}
-        </article>
-      ))}
+      {categories.map(({ name, drinks }) => {
+        return (
+          <section key={name} className={`${styles.section} row`}>
+            <h1>{name}</h1>
+
+            {drinks.map(({ name, price }, index) => (
+              <article key={`drink-${index}`} className="col-md-6">
+                <div>
+                  Nome: <span>{name}</span>
+                </div>
+                <div>
+                  Prezzo: <span>{price}</span>
+                </div>
+              </article>
+            ))}
+          </section>
+        );
+      })}
     </div>
   );
 }
