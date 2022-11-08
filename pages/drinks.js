bf7e93220accd0ad92a14aceb3ec65db4d0b593a
Matteo Bacci <matteo.bacci@nearform.com>
2022-11-08 20:56:04 +0100

    Initial commit

---
+++ pages/drinks.js
@@ -0,0 +1,32 @@
+import React from "react";
+import drinks from "../data/drinks.json";
+
+function Drink({ drinks }) {
+  return (
+    <div>
+      {drinks.map(({ name, price, category }, index) => (
+        <article key={`drink-${index}`}>
+          <div>
+            Nome: <span>{name}</span>
+          </div>
+          <div>
+            Prezzo: <span>{price}</span>
+          </div>
+          <div>
+            Categoria: <span>{category}</span>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      drinks,
+    }, // will be passed to the page component as props
+  };
+}
+
+export default Drink;
